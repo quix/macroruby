cffe62aeb7fa64b6c0b872921033471b444cd06d
quix <quixoticsycophant@gmail.com>
2009-02-23 01:33:46 -0500

    re-add mutual_ref.rb

---
+++ mutual_ref.rb
@@ -0,0 +1,38 @@
+
+def macro_swap(receiver, x, y)
+  tmp = MacroRuby.gen_string
+  %{
+    lambda {
+      #{tmp} = #{x}
+      #{x} = #{y}
+      #{y} = #{tmp}
+    }.call
+  }
+end
+
+def macro_test_1(receiver, x, y)
+  swap(receiver, x, y)
+  %{
+    p [#{x}, #{y}]
+  }
+end
+
+def macro_test_2(receiver, x, y)
+  %{
+    swap(#{x}, #{y})
+    p [#{x}, #{y}]
+  }
+end
+
+1.times {
+  a = 33
+  b = 44
+  test_1(a, b)
+}
+
+1.times {
+  a = 33
+  b = 44
+  test_2(a, b)
+}
+
